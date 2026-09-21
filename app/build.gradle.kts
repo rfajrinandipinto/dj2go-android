dd26773b953116176911046a788f2b2a9badecaf
rfajrinandipinto <nandipintorahmatfajri@gmail.com>
2026-09-21 15:17:12 +0700

    CI: install Android SDK packages, accept licenses, set sdk.dir; bump to 0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.dj2go"
         minSdk = 23
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.2"
+        versionCode = 3
+        versionName = "0.3"
     }
 
     signingConfigs {
